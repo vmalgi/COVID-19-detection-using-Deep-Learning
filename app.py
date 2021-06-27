e2c3fdcc4f68964eccb58b4734b8e86914a0ea09
Vinayakam19 <45694329+Vinayakam19@users.noreply.github.com>
2021-06-27 16:27:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH ='/Models/COVID19_VGG19.h5'
+MODEL_PATH ='/app/Models/COVID19_VGG19.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
